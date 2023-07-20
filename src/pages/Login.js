e623d9f756b6a9bc35a2c3d12e420e66bedf54d1
Gaurav singh bisht <gauravbisht82754@gmail.com>
2023-07-20 16:52:30 +0530

    myfirstreactcommit

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+export function Login() {
+    return(
+        <div>
+            <h1>This is login page</h1>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div className="Home">
+        <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
